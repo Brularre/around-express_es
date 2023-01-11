7f31b39d7dc051bd220bf4ae4fb3e5e7bfa23733
Brularre <bruno.larrain.e@gmail.com>
2023-01-10 22:11:01 -0300

    fix: remueve unos console.log

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ const { PORT = 3000 } = process.env;
 app.use('/', cardsRouter);
 app.use('/', usersRouter);
 app.use((req, res) => {
-  console.log({ message: 'Recurso solicitado no encontrado.' });
   res.status(404).send({ message: 'Recurso solicitado no encontrado.' });
 });
 
--- middlewares/readData.js
+++ middlewares/readData.js
@@ -8,15 +8,12 @@ const readData = (dataPath, selector, idPresent) => (req, res, next) => {
         const { id } = req.params;
         const user = JSON.parse(data).find((obj) => obj._id === id);
         if (!user) {
-          console.log({ message: 'ID de usuario no encontrado.' });
           res.status(404).send({ message: 'ID de usuario no encontrado.' });
           return;
         }
-        console.log(JSON.stringify(user));
         res.send(`Datos de usuari@ con id ${id}: Nombre: ${user.name}
       Descripción: ${user.about}, Link de su Avatar: ${user.avatar}`);
       } else {
-        console.log(data);
         res.send(`Datos de ${selector}: ${data}`);
         next();
       }
